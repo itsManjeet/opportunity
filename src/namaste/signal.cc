fccc30f991feb0a0fc6a2c47157fd7de97cabda1
itsmanjeet <itsmanjeet1998@gmail.com>
2020-08-29 22:25:51 +0530

    added signal->window::on_user_next_click() : to added user, && signal->window::on_get_start_click() : to quit application on completion

--- src/namaste/signal.cc
+++ src/namaste/signal.cc
@@ -11,11 +11,27 @@ window::on_wel_next_click()
 void
 window::on_user_next_click()
 {
-    __stack.set_visible_child("timezone_page");
+    int r = system(
+        io::sprint(
+            "useradd -g user -G sudo,wheel,netdev,storage,install,adm ",
+            " -p ", __user_page.__get_enc_passwd(), " -m ", __user_page.__get_user_id()
+        ).c_str()
+    );
+
+    if (WEXITSTATUS(r))
+        __exit_error("failed to add new user '" + __user_page.__get_user_id());
+    else
+        __stack.set_visible_child("timezone_page");
 }
 
 void
 window::on_timezone_next_click()
 {
     __stack.set_visible_child("getstart_page");
+}
+
+void
+window::on_get_start_click()
+{
+    this->get_application()->quit();
 }
