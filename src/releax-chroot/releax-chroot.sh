a07c82d5373aa0604bf78b1e76dea34bc85c5a1a
itsmanjeet <itsmanjeet1998@gmail.com>
2020-09-02 10:34:10 +0530

    added releax-chroot

---
+++ src/releax-chroot/releax-chroot.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+
+[[ -z $3 ]] && {
+    echo "software side error - no valid input provided"
+    return -2
+} 
+
+__work_dir=$1
+__bootloader_type=$2
+__ddir=$3
+
+
+
+function mount_fs {
+    mount --bind /dev $__work_dir/dev
+    mount -t devpts devpts $__work_dir/dev/pts -o gid=5,mode=620
+    mount -t proc proc $__work_dir/proc
+    mount -t sysfs sysfs $__work_dir/sys
+    mount -t tmpfs tmpfs $__work_dir/run
+
+    if [[ $__bootloader_type == "EFI" ]] ; then
+        mount --bind /sys/firmware/efi/efivars $__work_dir/sys/firmware/efi/efivars
+    fi
+}
+
+function umount_fs {
+    umount $__work_dir/dev/pts
+    umount $__work_dir/dev
+    umount $__work_dir/run
+    umount $__work_dir/proc
+    if [[ $__bootloader_type == "EFI" ]] ; then
+        umount $__work_dir/sys/firmware/efi/efivars
+    fi
+    umount $__work_dir/sys 
+}
+
+
+
+mount_fs
+if [[ $__bootloader_type == "EFI" ]] ; then
+    chroot $__work_dir grub-install --target=x86_64-efi --bootloader-id=releax --recheck &>/dev/null
+else
+    chroot $__work_dir grub-install &>/dev/null
+fi
+__r=$?
+
+if [[ $__r -ne 0 ]] ; then
+    echo "failed to install bootloader"
+    exit $__r
+fi
+
+chroot $__work_dir grub-mkconfig -o /boot/grub/grub.cfg &>/dev/null
+__r=$?
+
+umount_fs
+
+
+exit __$r
